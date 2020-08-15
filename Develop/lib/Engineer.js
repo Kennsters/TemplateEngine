735ff4b59d8a5703801fabfeeea78f4f9c798e49
Kenny Yang <kennyyangh@gmail.com>
2020-08-15 01:41:37 -0700

    created ignore file and constructors

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee.js')
+
+class Engineer extends Employee {
+    constructor (name, id, email, githubUser) {
+        super(name, id, email)
+        this.githubUser = githubUser
+        this.title = 'Engineer'
+    }
+    getgithubUser () {
+        return this.githubUser
+    }
+    getTitle () {
+        return this.title
+    }
+}
+
+module.exports = Engineer
